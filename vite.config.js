34f5f697b59986772aa29b4a2e0ca299f7b3adf9
raul.jimenez <raul.jimenez@soprasteria.com>
2023-06-07 13:56:59 +0200

    fix: vite config manifest & sw

--- vite.config.js
+++ vite.config.js
@@ -4,16 +4,18 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { VitePWA } from 'vite-plugin-pwa';
+import manifest from './manifest.webmanifest';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     VitePWA({
-      manifest: './manifest.json',
+      manifest,
       registerType: 'autoUpdate',
       workbox: {
-        globPatterns: ['**/*.{js,css,ttf,jpg,png}'],
+        globIgnores: ['index.html'],
+        globPatterns: ['**/*.{js,css,ttf,jpg,png,html}', 'fonts'],
         globDirectory: 'dist',
       },
     }),
