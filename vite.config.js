497d22d71ac38ebb2314e96e0cfa66fe5361d4f8
raul.jimenez <raul.jimenez@soprasteria.com>
2023-06-07 14:06:14 +0200

    fix: webmanifest

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { VitePWA } from 'vite-plugin-pwa';
-import manifest from './manifest.webmanifest';
+import manifest from './manifest.json';
 
 // https://vitejs.dev/config/
 export default defineConfig({
